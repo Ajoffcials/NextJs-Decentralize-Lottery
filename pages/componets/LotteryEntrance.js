8932c6ec555b2ba076cb5a46a510886342bfb6ad
Blockchain Oracle <Abubakrjimoh16488@gmail.com>
2024-01-06 17:22:37 +0100

    first commit

---
+++ pages/componets/LotteryEntrance.js
@@ -0,0 +1,129 @@
+import { useWeb3Contract } from "react-moralis";
+import { abi, contractAddress } from "../../constants/index";
+import { useMoralis } from "react-moralis";
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import { useNotification } from "web3uikit";
+
+export default function LotteryEntrance() {
+  const { chainId: chainIdhex, isWeb3Enabled } = useMoralis();
+  const chainId = parseInt(chainIdhex);
+  const raffleAddress =
+    chainId in contractAddress ? contractAddress[chainId][0] : null;
+
+  const [entranceFee, setEntranceFee] = useState("0");
+  const [numPlayers, setPlayers] = useState("0");
+  const [recentWinner, setRecentWinner] = useState("0");
+
+  const dispatch = useNotification();
+
+  const {
+    runContractFunction: enterRaffle,
+    isLoading,
+    isFetching,
+  } = useWeb3Contract({
+    abi: abi,
+    contractAddress: raffleAddress,
+    functionName: "enterRaffle",
+    params: {},
+    msgValue: entranceFee,
+  });
+
+  const { runContractFunction: getEntranceFee } = useWeb3Contract({
+    abi: abi,
+    contractAddress: raffleAddress,
+    functionName: "getEntranceFee",
+    params: {},
+  });
+
+  const { runContractFunction: getNumberOfPlayers } = useWeb3Contract({
+    abi: abi,
+    contractAddress: raffleAddress,
+    functionName: "getNumberOfPlayers",
+    params: {},
+  });
+  const { runContractFunction: getWinner } = useWeb3Contract({
+    abi: abi,
+    contractAddress: raffleAddress,
+    functionName: "getWinner",
+    params: {},
+  });
+
+  async function updateUi() {
+    const entranceFeeRes = (
+      await getEntranceFee({
+        onError: (error) => console.log(error),
+      })
+    ).toString();
+    setEntranceFee(entranceFeeRes);
+    console.log(ethers.utils.formatUnits(entranceFeeRes, "ether"));
+
+    const getNumberOfPlayersRes = (
+      await getNumberOfPlayers({
+        onError: (error) => console.log(error),
+      })
+    ).toString();
+    setPlayers(getNumberOfPlayersRes);
+
+    const getWinnerRes = (
+      await getWinner({
+        onError: (error) => console.log(error),
+      })
+    ).toString();
+    setRecentWinner(getWinnerRes);
+  }
+
+  useEffect(() => {
+    if (isWeb3Enabled) {
+      updateUi();
+    }
+  }, [isWeb3Enabled]); //this starts as false
+
+  const handleSuccess = async function (tx) {
+    await tx.wait(1);
+    handleNotification(tx);
+    updateUi();
+  };
+
+  const handleNotification = function () {
+    dispatch({
+      type: "info",
+      message: "transaction  Complete!",
+      title: "Tx Notification",
+      position: "topR",
+      // icon: "bell",
+    });
+  };
+  return (
+    <div className="p-5">
+      Hi from Lottery Entrance
+      {raffleAddress ? (
+        <div>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto"
+            onClick={async () =>
+              await enterRaffle({
+                // onComplete:
+                // onError:
+                onSuccess: handleSuccess,
+                onError: (error) => console.log(error),
+              })
+            }
+            disabled={isLoading || isFetching}
+          >
+            {isLoading || isFetching ? (
+              <div className="animate-spin spinner-border h-8 w-8 border-b-2 rounded-full"></div>
+            ) : (
+              "Enter Raffle"
+            )}
+          </button>
+          <div>Entrance Fee: {ethers.utils.formatEther(entranceFee)} ETH</div>
+          <div>Number Of Players: {numPlayers}</div>
+          <div>Recent Winner: {recentWinner}</div>
+        </div>
+      ) : (
+        <div>No address Found</div>
+      )}
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+// import MannualHeader from "./componets/MannualHeader";
+import Header from "./componets/Header";
+import LotteryEntrance from "./componets/LotteryEntrance";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Smart Contract Lottery</title>
+        <meta name="description" content="Our smart contract Lottery" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <LotteryEntrance />
+    </div>
+  );
+}
